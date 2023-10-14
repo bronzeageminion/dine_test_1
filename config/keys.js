bd824a4ea2f3a76545879dec5e96e690b53612d4
bronzeageminion <142891589+bronzeageminion@users.noreply.github.com>
2023-10-15 03:55:51 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod_keys')
-} else {
-    module.exports = require('./dev_keys')
-}
+module.exports = {
+  249384962636-sqrh53iba4q0lkca685lnhqjt0dvajep.apps.googleusercontent.com: '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',
+  // ... other keys
+};
